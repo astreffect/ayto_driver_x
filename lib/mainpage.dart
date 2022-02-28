8706f2afbb03d8e3b26434f1185e52259d713f02
Debarun Acharya <debarunacharya2002@gmail.com>
2022-02-28 15:04:54 +0530

    Mainscreen Progress v2 (Incomplete)unversioned

---
+++ lib/mainpage.dart
@@ -0,0 +1,84 @@
+import 'package:ayto_driver_x/brand_colors.dart';
+//import 'package:cabdriver/globalvariabels.dart';
+//import 'package:cabdriver/tabs/earningstab.dart';
+//import 'package:ayto_driver_x/tabs/hometab.dart';
+import 'package:ayto_driver_x/tabs/profiletab.dart';
+import 'package:ayto_driver_x/tabs/ratingstab.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  static const String id = 'mainpage';
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> with SingleTickerProviderStateMixin {
+
+ // TabController tabController;
+  int selecetdIndex = 0;
+
+  void onItemClicked(int index){
+    setState(() {
+      selecetdIndex = index;
+      //tabController.index = selecetdIndex;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+   // tabController = TabController(length: 4, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+   // tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+     /* body: TabBarView(
+        physics: NeverScrollableScrollPhysics(),
+      /*  controller: tabController,
+        children: <Widget>[
+          HomeTab(),
+          EarningsTab(),
+          RatingsTab(),
+          ProfileTab(),
+        ],*/
+      ),*/
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.credit_card),
+            title: Text('Earnings'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            title: Text('Ratings'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: Text('Account'),
+          ),
+        ],
+        currentIndex: selecetdIndex,
+        unselectedItemColor: BrandColors.colorAccent,
+        selectedItemColor: BrandColors.colorOrange,
+        showUnselectedLabels: true,
+        selectedLabelStyle: TextStyle(fontSize: 12),
+        type: BottomNavigationBarType.fixed,
+        onTap: onItemClicked,
+      ),
+    );
+  }
+}
